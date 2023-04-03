e3c68900d3a93c150de8159e48b4486f6f397f3a
Dương Thăng Long <91776828+duongthanglong@users.noreply.github.com>
2023-04-03 17:22:53 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, make_response, request, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+   user_agent = request.user_agent
+   sclient = 'Client address:'
+   if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+      sclient += request.environ['REMOTE_ADDR']
+   else:
+      sclient += request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
+   sclient += f';Platform:{user_agent.platform};Browser:{user_agent.browser}'
+   return render_template('index.html', client_request=sclient)
